61254335e194d9ace7071a9e9216655157acfb93
SpagettBro <markgonzalez02@hotmail.com>
2024-10-17 10:07:39 +0200

    gekke push

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import BlockN from "./components/BlockN"
+import BlockM from "./components/BlockM"
+import BlockL from "./components/BlockL"
+
+export default function Home() {
+  return (
+    <div>
+      <BlockR/>
+    </div>
+  );
+}
